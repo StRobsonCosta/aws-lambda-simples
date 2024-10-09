6ca678b918e6b70e9970bfd98eff73b7e200b3d9
Robson Costa <st.robson.costa@gmail.com>
2024-10-09 16:02:11 -0300

    Regra para definição Lambda

--- index.mjs
+++ index.mjs
@@ -1,10 +1,23 @@
-import { log } from './log.mjs'
+import { log } from './log.mjs';
+import { S3 } from '@aws-sdk/client-s3';
 
-export const handler = async (event) => {
-  log('event: ' + JSON.stringify(event));
+const s3Client = new S3({ region: 'us-west-2' });
+export const handler = async(event) => {
+    const record = event.Records[0];
+    const Bucket = record.s3.bucket.name;
+    const Key = record.s3.object.key;
+    const getObjectResult = await s3Client.getObject({
+        Bucket,
+        Key,
+    });
+    const mega_byte = 1024 * 1024;
 
-  return {
-      statusCode: 200,
-      body: JSON.stringify(event),
-  }
+    if (getObjectResult.ContentLength > 1 * mega_byte) {
+        log('Objeto muito grande');
+
+        return 'Objeto muito grande';
+    }
+
+    log('Objeto de tamanho OK');
+    return 'Objeto de tamanho OK';
 };
